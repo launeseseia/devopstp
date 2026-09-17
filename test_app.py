a0b7f9173b7a043dc7de247701559e2818f241c3
launeseseia <martin.launes@et.esiea.fr>
2026-09-17 16:27:10 +0200

    test: etape 7 2nd try

--- test_app.py
+++ test_app.py
@@ -19,5 +19,5 @@ def test_health_endpoint():
 def test_status_endpoint():
     client = app.test_client()
     response = client.get("/status")
-    assert response.status_code == 200
+    assert response.status_code == 5
     assert response.get_json()["service"] == "projet-devops-groupe-demo"
