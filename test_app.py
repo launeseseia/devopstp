206133f2f3992c8098556d5a9d21bafbcb6a8457
launeseseia <martin.launes@et.esiea.fr>
2026-09-17 15:30:02 +0200

    fix /status

--- test_app.py
+++ test_app.py
@@ -20,4 +20,4 @@ def test_status_endpoint():
     client = app.test_client()
     response = client.get("/status")
     assert response.status_code == 200
-    assert response.get_json()["status"] == "ok"
+    assert response.get_json()["service"] == "projet-devops-groupe-demo"
